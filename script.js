99abe47e1ea98ffe9b39ccc8bc1c3944a404db88
HK <hkb202518@gmail.com>
2026-05-12 16:45:08 +0530

    Used array, replaced addTask() with renderTask(), moved from DOM manipulation to "rendering from Data" UI

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 console.log("JS connected");
+
 const input = document.getElementById("taskInput");
 const button = document.getElementById("addBtn");
 const list = document.getElementById("taskList");
+
+let tasks = [];
+
 button.addEventListener("click", function () {
 
     console.log("Add Button clicked");
@@ -11,30 +15,47 @@ button.addEventListener("click", function () {
         alert("Please enter a task");
         return;
     }
-    addTask(taskText);
+    tasks.push({
+        id:Date.now(),
+        text: taskText,
+        completed:false
+    })
+    renderTasks();
 
     input.value = "";
 });
 
-function addTask(taskText) {
-    
-    console.log("Task to add:", taskText);
+function renderTasks() {
+    list.innerHTML = "";
+    tasks.forEach(function (task) {
+        const li = document.createElement("li");
+        li.textContent = task.text;
 
-    const li = document.createElement("li");
-    li.textContent = taskText;   
-    li.addEventListener("click", function () {        
-        console.log("Task clicked:", taskText);
-        li.classList.toggle("completed");
+        if (task.completed) {
+            li.classList.add("completed")
+        }
+
+        li.addEventListener("click", function () {
+            console.log("Task clicked:", task.text);
+            task.completed = !task.completed;
+
+            renderTasks()
+        })
+        
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "Delete";
+
+        deleteBtn.addEventListener("click", function (event) {
+            event.stopPropagation();
+            console.log("Delete button clicked for task:", task.text);
+
+            tasks = tasks.filter(function (taskItem) {
+                return taskItem.id !== task.id;
+            });
+
+            renderTasks()
+        })
+        li.appendChild(deleteBtn);
+        list.appendChild(li);
     })
-
-    const deleteBtn = document.createElement("button");
-    deleteBtn.textContent = "Delete";
-    deleteBtn.addEventListener("click", function (event) {
-        event.stopPropagation();
-        console.log("Delete button clicked for task:", taskText);
-        li.remove();
-    })
-
-    list.appendChild(li);
-    li.appendChild(deleteBtn);     
 }
